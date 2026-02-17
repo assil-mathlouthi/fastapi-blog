3e6286ce80caa4656345f89d88e8141a24708c89
Assil <assil.mathlouthi@etudiant-isi.utm.tn>
2026-02-17 12:53:12 +0100

    add posts endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request,requests
+from fastapi import FastAPI, Request,HTTPException,status
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -29,3 +29,13 @@ def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request, "posts": posts,"title":"Vortex"})
    
 
+@app.get("/posts")
+def get_posts():
+    return posts
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int):
+    post = next((post for post in posts if post["id"] == post_id),None)
+    if post:
+        return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{post_id} not found")
