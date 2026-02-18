45ec44ebee45548889abb341ac9d899782f009ae
Assil <assil.mathlouthi@etudiant-isi.utm.tn>
2026-02-18 22:28:05 +0100

    add user and post models

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = "sqlite:///./blog.db"
+
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    with session_local() as db:
+        yield db
