bcdd814d55477c05b15acebc19d9aed99eafaff4
Assil <assil.mathlouthi@etudiant-isi.utm.tn>
2026-02-18 22:47:47 +0100

    add user schema

--- schemas.py
+++ schemas.py
@@ -1,15 +1,33 @@
-from pydantic import BaseModel , ConfigDict, Field
+from datetime import datetime
+from pydantic import BaseModel , ConfigDict, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1,max_length=50)
+    email: EmailStr = Field(max_length=100)
+    password: str = Field(min_length=6)
+
+class UserCreate(UserBase):
+    ...
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id : int
+    image_file : str | None
+    image_path : str
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1,max_length=100)
     content: str = Field(min_length=1)
-    author : str = Field(min_length=1,max_length=50)
     
 class PostCreate(PostBase):
-    ...
+    user_id: int
     
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     
     id : int
-    date_posted : str
+    date_posted : datetime
+    author : UserResponse
